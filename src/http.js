a81e025d5905d586097841d501b63db6731d1097
YeaminRabbi <yeaminrabbi308@gmail.com>
2024-11-24 01:14:21 +0600

    fetch blogs

---
+++ src/http.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export default axios.create({
+    
+    // baseURL: "https://blog.techstringit/api"
+    baseURL: "http://127.0.0.1:8000/api",
+    headers:{
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    }
+    
+})
---
+++ src/pages/home.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import http from "../http"
+import { Link } from "react-router-dom";
+
+export default function Home() {
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        fetchAllBlogs();
+    }, []);
+
+    const fetchAllBlogs = () => {
+        http.get('/blogs').then(response => {
+            console.log(response.data.blogs);
+            
+            setBlogs(response.data.blogs); // Set the appropriate part of the response
+        }).catch(error => {
+            console.error("Error fetching blogs:", error);
+        });
+    }
+    return (
+
+        <div>
+            <h2>Blog listing ...</h2>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Sno.</th>
+                        <th>Image</th>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Date</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {blogs.map((blog, index) => (
+                        <tr key={blog.id}>
+                            <td>{++index}</td>
+                            <td>
+                                <img src={blog.image} alt={blog.title} className="imgWidth" />
+                            </td>
+                            <td>{blog.title}</td>
+                            <td>{blog.author.name}</td>
+                            <td>{blog.created_at}</td>
+                        </tr>
+                    ))}
+
+
+                </tbody>
+            </table>
+        </div>
+
+    )
+}
